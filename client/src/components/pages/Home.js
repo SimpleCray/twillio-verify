22bae4a79ff78c277f01fde9bcd367fb9fe503fa
Hai Duong <hai.duong@kodebaze.com>
2022-07-22 18:41:58 +0700

    add form

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
-
+    const { test } = useSelector((state) => state.app);
     return (
-        <div>
-            Home page
+        <div className='radial-background home-page'>
+            <div className='container'>
+                <div className='form'>
+                    <div className='header'>Enter your phone number: </div>
+                    <input type='number' />
+                    <button>Send Code</button>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Home;

e9e1f5af1662d5b8c826c3b2c69122a0537fe461
Hai Duong <ngochaiitech@gmail.com>
2022-07-23 11:06:14 +0700

    add readme

---
+++ README.md
@@ -0,0 +1,46 @@
+# Twilio Phone Verify App
+
+This project was built with client - server structure by [Hai Duong](https://github.com/SimpleCray).
+To run this app you need to install NodeJS and NPM
+
+
+## Client & Setup (folder client)
+Made with [Create React App](https://github.com/facebook/create-react-app).
+
+### Setup environment variables
+Create new .env file with the content copied from file .env.development.
+
+### Scripts
+In the project directory, you can run:
+
+#### `npm install`
+This command installs a package and any packages that it depends on.
+
+#### `npm start`
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+
+## Server & Setup (folder server)
+Made with ExpressJS.
+
+### Setup environment variables
+Create new .env file with the content copied from file .env.development.
+
+### Scripts
+In the project directory, you can run:
+
+#### `npm install`
+This command installs a package and any packages that it depends on.
+
+#### `npm start`
+Runs the app in the development mode.\
+Server will start on Host [http://localhost:4000](http://localhost:4000).
+
+
+## Contact
+
+For further information, please contact:
+### Email: ngochaiitech@gmail.com
+[LinkedIn](https://www.linkedin.com/in/simplecray/).
+[Github](https://www.gihub.com/simplecray/).
--- server/services/verify/index.js
+++ server/services/verify/index.js
@@ -1,46 +1,6 @@
 import { Router } from 'express';
 require('dotenv').config();
 
-// const router = Router();
-// const twilio = require('twilio');
-// const serviceSid = process.env.SERVICE_SID;
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
-// const client = new twilio(accountSid, authToken);
-
-// router.get('/test', async (req, res, next) => {
-//     try {
-//         console.log('get in')
-//         client.verify.v2
-//             .services(serviceSid)
-//             .verifications.create({ to: '+84375911341', channel: 'sms' })
-//             .then((verification) => {
-//                 console.log('return')
-//                 console.log(verification.sid);
-//                 res.status(200).send(data);
-//             });
-//     } catch (error) {
-//         console.log(error)
-//         res.status(500).json({ message: 'Test failed' });
-//     }
-// });
-
-// const router = Router();
-// const twilio = require('twilio');
-// const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, VERIFICATION_SID } = process.env;
-// const client = twilio('AC5d39305087853c73a159206f5853e564', '85eab5e4fefa5ff52098497335586443');
-
-// router.get('/get-code', async (req, res, next) => {
-//     let verificationRequest;
-
-//     try {
-//         verificationRequest = await client.verify.services('VA143a44056f2579ad70b998f0b3ee540f').verifications.create({ to: '+84375911341', channel: 'sms' });
-//         return res.status(200).send({message: 'Code sent!'});
-//     } catch (e) {
-//         return res.status(500).send(e);
-//     }
-// });
-
 const router = Router();
 const twilio = require('twilio');
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, VERIFICATION_SID } = process.env;
@@ -64,7 +24,6 @@ router.post('/check-code', async (req, res, next) => {
     let verificationResult;
     try {
         verificationResult = await client.verify.services(VERIFICATION_SID).verificationChecks.create({ to, code });
-        console.log(verificationResult)
         if (verificationResult.status === 'approved') {
             return res.status(200).send({verified: true, message: 'Phone number verified!'});
         } else {
